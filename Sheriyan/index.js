3ae31f9ddb6cb99d2843cdb27dceeac277030785
BRP7 <bhumiparmar0007@gmail.com>
2024-04-21 17:49:39 +0530

    Middleware

--- Sheriyan/index.js
+++ Sheriyan/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+
+app.use(function(req,res,next){
+    console.log("Middleware Running!");
+    //res.send("Hello From Middleware");//Middleware Running!
+    //Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    next();
+})
 //home page(base url routing)
 app.get('/',function(req,res,next){
     res.send("Hello World");
