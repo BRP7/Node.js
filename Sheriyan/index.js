37237d4cdf56325bca89e83b5a7047d712b01734
BRP7 <bhumiparmar0007@gmail.com>
2024-04-21 17:44:41 +0530

    Dynamic Routing

--- Sheriyan/index.js
+++ Sheriyan/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+//home page(base url routing)
 app.get('/',function(req,res,next){
     res.send("Hello World");
 })
 
+//dynamic routing
+app.get('/profile/:username',function (req,res) {
+    res.send("Hello, "+ req.params.username);
+})
+
+//app running on port 3000
 app.listen(3000);
